d51d424f7e11886b23d5d50c08234844e510b9c4
Mahmoud Elshall <54082956+mahmoudelshall@users.noreply.github.com>
2023-11-24 01:54:17 +0200

    Authentication completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,26 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
+
     public function register(Request $request)
     {
-        
+
+        // data validation
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|min:6"
+        ]);
+
+        // User Model
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+
+        // Response
+        return response()->json([
+            "status" => true,
+            "message" => "User registered successfully"
+        ]);
     }
 
     public function login(Request $request)
     {
-       
-        
-        
+
+        // data validation
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        // JWTAuth
+        $token = JWTAuth::attempt([
+            "email" => $request->email,
+            "password" => $request->password
+        ]);
+
+        if (!empty($token)) {
+
+            return response()->json([
+                "status" => true,
+                "message" => "User logged in succcessfully",
+                "token" => $token
+            ]);
+        }
+
+        return response()->json([
+            "status" => false,
+            "message" => "Invalid Credentials"
+        ]);
     }
 
-    public function logout(Request $request)
+    public function logout()
     {
-       
-        
-        
+
+        auth()->logout();
+
+        return response()->json([
+            "status" => true,
+            "message" => "User logged out successfully"
+        ]);
     }
 }
